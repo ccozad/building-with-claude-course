01b5b51ba1f88b114de0f0120d4c3894c3250ad3
ccozad <ccozad@gmail.com>
2026-03-26 01:10:07 -0700

    generate eval data script

---
+++ generate-eval-dataset.py
@@ -0,0 +1,73 @@
+import json
+from dotenv import load_dotenv
+load_dotenv()
+
+from anthropic import Anthropic
+
+client = Anthropic()
+model = "claude-haiku-4-5"
+
+def add_user_message(messages, text):
+    user_message = {
+        "role": "user",
+        "content": text
+    }
+    messages.append(user_message)
+
+def add_assistant_message(messages, text):
+    assistant_message = {
+        "role": "assistant",
+        "content": text
+    }
+    messages.append(assistant_message)
+
+def chat(
+    messages, 
+    system_prompt=None, 
+    temperature=0.7, 
+    stop_sequences=[]):
+    params = {
+        "model": model,
+        "max_tokens": 1000,
+        "messages": messages,
+        "temperature": temperature
+    }
+
+    if system_prompt:
+        params["system"] = system_prompt
+    if stop_sequences:
+        params["stop_sequences"] = stop_sequences
+
+    response = client.messages.create(**params)
+    return response.content[0].text
+
+def generate_dataset():
+    prompt = """
+Generate an evaluation dataset for a prompt evaluation. The dataset will be used to evaluate prompts that generate Python, JSON, or Regex specifically for AWS-related tasks. Generate an array of JSON objects, each representing task that requires Python, JSON, or a Regex to complete.
+
+Example output:
+```json
+[
+  {
+    "task": "Description of task",
+  },
+  ...additional
+]
+```
+
+* Focus on tasks that can be solved by writing a single Python function, a single JSON object, or a single regex
+* Focus on tasks that do not require writing much code
+
+Please generate 3 objects.
+"""
+
+    messages = []
+    add_user_message(messages, prompt)
+    add_assistant_message(messages, "```json")
+    response = chat(messages, stop_sequences=["```"])
+    return json.loads(response)
+
+if __name__ == "__main__":
+    dataset = generate_dataset()
+    with open("eval_dataset.json", "w") as f:
+        json.dump(dataset, f, indent=2)
